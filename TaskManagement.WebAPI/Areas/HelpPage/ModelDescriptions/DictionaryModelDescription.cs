b7ea94b61814a11b43177ce0281c86d04e20147e
Kelvin Paa Kwesi Adjei <kpkadjei1@gmail.com>
2021-09-24 14:42:33 -0400

    Add project files.

---
+++ TaskManagement.WebAPI/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace TaskManagement.WebAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ TaskManagement.WebAPI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TaskManagement.WebAPI.WebApiApplication" Language="C#" %>
